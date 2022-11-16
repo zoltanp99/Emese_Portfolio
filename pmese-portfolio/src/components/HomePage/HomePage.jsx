02358130a32d19b5b24bfab00bd11b4a60adc3e4
zoltanp <zoltanp1234@gmail.com>
2022-11-16 01:06:45 +0100

    - final commit

--- pmese-portfolio/src/components/HomePage/HomePage.jsx
+++ pmese-portfolio/src/components/HomePage/HomePage.jsx
@@ -6,6 +6,16 @@ export const HomePage = (props) => {
     const biographyRef = useRef(null);
     const navigate = useNavigate();
 
+    const aboutMe = `
+    My name is Emese Ban, I am a graphics designer.
+    
+    Currently I am attending my senior year, majoring in Graphical Engineering and Design at the Higher Education Technical School of Professional Studies in Novi Sad. Before I started studying to be a designer, i worked as a goldsmith's assistant at the "Šiket" Jewelry store, also in Novi Sad.
+    
+    I like to think I am devoted to my work, open minded, and a team-player. I get across to people and adjust to changes with ease. My belief is that a person always has to strive to get better and evolve professionally, but also keep improving on oneself.
+    
+    Currently I am open to new possibilities in my career which would help me refine my skills in both art and design.
+    `
+
     const handleClick = () =>{
         biographyRef.current?.scrollIntoView({behavior: 'smooth'})
     };
@@ -26,9 +36,36 @@ export const HomePage = (props) => {
         </div>
         <div className={styles.biographyContainer} ref={biographyRef}>
             <h1>About Me</h1>
-            <div style={{'padding': '5px'}}>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            <h2>
+                <i>My name is Emese Ban, I am a graphics designer.</i>
+            </h2>
+            <div className={styles.paragraphContainer}>
+                <p>
+                    Currently I am attending my senior year at university, majoring in Graphical Engineering and Design at the <i>Higher Education Technical School of Professional Studies</i> in Novi Sad. Before I started studying to be a designer, i worked as a goldsmith's assistant at the <em>Šiket</em> Jewelry store, also in Novi Sad.
+                </p>
             </div>
+            <div className={styles.paragraphContainer}>
+                <p>
+                    I like to think I am devoted to my work, open minded, and a team-player. I get across to people and adjust to changes with ease. My belief is that a person always has to strive to get better and evolve professionally, but also keep improving on oneself.
+                </p>
+            </div>
+            <div className={styles.paragraphContainer}>
+                <p>
+                    Currently I am open to new possibilities in my career which would help me refine my skills in both art and design.
+                </p>
+            </div>
+        <div className={styles.footer}>
+            <div className={styles.emeseSocials}>
+                <span>Contact:</span>
+                <a href='mailto:emeseban5@gmail.com' className={styles.contactText}>emeseban5@gmail.com</a>
+            </div>
+            <div className={styles.emeseSocials}>
+                <span>Socials</span>
+                <a href='https://www.instagram.com/ema_ban_/' className={styles.contactText}>Instagram</a>
+                <a href='https://www.facebook.com/emese.ban.3' className={styles.contactText}>Facebook</a>
+                <a href='https://rs.linkedin.com/' className={styles.contactText}>LinkedIn</a>
+            </div>
+        </div>
         </div>
       </div>
     );
